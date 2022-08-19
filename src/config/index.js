2d1a5aa1d53f7f2d41bb9f7651aba386404e6035
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-08-19 03:02:23 +0100

    added env variables for gmail OAuth

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,13 @@
-require('dotenv').config()
-
+require("dotenv").config();
 
 module.exports = {
-    mongoURI: process.env.MONGO_DB_URI,
-    PORT: process.env.PORT
-}
+  MONGO_DB_URI: process.env.MONGO_DB_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  TOKEN_EXPIRY: process.env.TOKEN_EXPIRATION_TIME,
+  PORT: process.env.PORT,
+  MAIL_USERNAME: process.env.MAIL_USERNAME,
+  MAIL_PASSWORD: process.env.MAIL_PASSWORD,
+  OAUTH_CLIENTID: process.env.OAUTH_CLIENTID,
+  OAUTH_CLIENT_SECRET: process.env.OAUTH_CLIENT_SECRET,
+  OAUTH_REFRESH_TOKEN: process.env.OAUTH_REFRESH_TOKEN,
+};
