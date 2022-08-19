55cb9b89c1e1faf347e7491ec565c6a951fcb506
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-08-19 03:15:22 +0100

    finished up functions

--- src/services/jwtService.js
+++ src/services/jwtService.js
@@ -2,16 +2,25 @@ const jwt = require("jsonwebtoken");
 const { TOKEN_EXPIRY } = require("../config");
 const { JWT_SECRET } = require("../config");
 
-exports.createToken = (user) => {
-    const payload = {
-        id: user._id,
-        username: user.username,
-        email: user.email,
-        role: user.role,
-    };
-    const options = {
-        expiresIn: parseInt(TOKEN_EXPIRY),
-    };
-    return jwt.sign(payload, JWT_SECRET, options);
-    }
- 
+exports.generateToken = (user) => {
+  const payload = {
+    id: user._id,
+    username: user.username,
+    email: user.email,
+    role: user.role,
+  };
+  const options = {
+    expiresIn: parseInt(TOKEN_EXPIRY),
+  };
+  return jwt.sign(payload, JWT_SECRET, options);
+};
+
+exports.decodeToken = (token) => {
+  try {
+    const decodedToken = jwt.verify(token, JWT_SECRET);
+    return decodedToken;
+  } catch (err) {
+    console.log(err.message);
+    return;
+  }
+};
