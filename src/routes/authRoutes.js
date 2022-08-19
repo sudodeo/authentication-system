4651df805640204769f817680bfcabcf5732569e
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-08-19 03:23:51 +0100

    added middleware to routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const authController = require("../controllers/authController");
+const authMiddleware = require("../middleware/auth");
+const {validateAuthRequest, authValidationResult} = require("../middleware/validator");
 
 router
   .post("/login", authController.postLogin)
-  .post("/register", authController.postRegister);
-
+  .post("/register", validateAuthRequest, authValidationResult, authController.postRegister)
+  .post("/resetpassword", authController.postPasswordReset)
+  .put("/resetpassword/:token", authController.putPasswordReset)
 
 module.exports = router
